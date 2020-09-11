aa644c5825e2e2edea1e7bfcd6021fba6a3900de
KeshavGarg01 <47708838+KeshavGarg01@users.noreply.github.com>
2020-09-11 20:16:12 +0530

    Update and rename memorymap to memorymap.c

---
+++ memorymap.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h> 
+  
+int main(void) 
+{ 
+    return 0; 
+} 
+*/
+#include <stdio.h> 
+  
+int global; // Uninitialized variable stored in bss
+  
+int main(void) 
+{ 
+    return 0; 
+} 
