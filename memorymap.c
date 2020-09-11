ea57a9246b5f586759d016bc08c90f20db75a594
KeshavGarg01 <47708838+KeshavGarg01@users.noreply.github.com>
2020-09-11 20:18:03 +0530

    Update memorymap.c

--- memorymap.c
+++ memorymap.c
@@ -19,8 +19,9 @@ int main(void)
 /*
 #include <stdio.h> 
   
-int global; /* Uninitialized variable stored in bss*/
-  
+int global; // Uninitialized variable stored in bss
+*/
+/*
 int main(void) 
 { 
     static int i = 100; /* Initialized static variable stored in DS*/
